34da1f590f670ba6dd1db816d90b813ed0391179
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-03-25 15:13:42 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,34 @@
 variable "project_id" {
   description = "GCP Project Id"
+  default = "opsnow-qa-automation"
 }
 
 variable "region" {
   description = "GCP Region"
+  default = "asia-northeast3"
 }
 
 variable "zone" {
   description = "GCP Region Zone"
+  default = "asia-northeast3-a"
 }
 
 variable "instance_name" {
   description = "VM Instance Name"
+  default = "terraform-external-hi-instance"
 }
 
 variable "instance_type" {
   description = "VM Instance Type"
+  default = "e2-micro"
 }
 
 variable "instance_image" {
   description = "VM Instance Image"
+  default = "debian-cloud/debian-9"
 }
 
 variable "vpc_name" {
   description = "Virtual Network Name"
+  default = "terraform-external-hi-network"
 }
